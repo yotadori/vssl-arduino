b2d4dc0b7dca9d947787f36bb42e2f4bc675b800
yotadori <yotakunhappy@gmail.com>
2026-09-13 10:30:06 +0900

    include追加

--- vssl_arduino.ino
+++ vssl_arduino.ino
@@ -6,6 +6,7 @@
 #include "src/Robot/Robot.h"
 #include "src/Speaker/Speaker.h"
 #include "src/Gyro/Gyro.h"
+#include "src/XboxControllerReceiver/XboxControllerReceiver.h"
 
 // ピン番号
 constexpr int SERVO_PIN = D3;
